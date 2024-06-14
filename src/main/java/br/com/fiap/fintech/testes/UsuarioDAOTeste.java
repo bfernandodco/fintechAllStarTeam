5f3ee5033d062310ec6c72b4bf512bd6033e00e6
Andre Victor <andrevictordorego@gmail.com>
2024-06-14 16:20:41 -0300

    validacao de cpf

--- src/main/java/br/com/fiap/fintech/testes/UsuarioDAOTeste.java
+++ src/main/java/br/com/fiap/fintech/testes/UsuarioDAOTeste.java
@@ -1,6 +1,7 @@
 package br.com.fiap.fintech.testes;
 
 import java.io.File;
+import java.sql.Date;
 import java.time.LocalDate;
 
 import br.com.fiap.fintech.bean.Usuario;
@@ -14,15 +15,19 @@ public class UsuarioDAOTeste {
 		String caminhoImg = "C:\\Users\\andre\\OneDrive\\Área de Trabalho\\NovaImagemBanco2.jpg";
 		File imagem = new File (caminhoImg);
 		
-		Usuario usuario = new Usuario(11111111127L, "RenatASoares", LocalDate.of(1991, 03, 28), "Masculino", "RenatASoares@email.com", null, "jk5689");
-		Usuario usuarioTesteEditado = new Usuario(33344466681L, "Novo contato", LocalDate.of(1991, 9, 10), "Feminino", "mel@email.com", null, "12X3456");
+		Long cpfValido = 41300703806L;
+		Long cpfInvalido = 12345678900L;
+		
+		Usuario usuario = new Usuario(cpfValido, "RenatASoares", new Date(91, 2, 28), "Masculino", "RenatASoares@email.com");
+		
+		//Usuario usuarioTesteEditado = new Usuario(33344466681L, "Novo contato", LocalDate.of(1991, 9, 10), "Feminino", "mel@email.com", null, "12X3456");
 		
 		UsuarioDAO dao = new OracleUsuarioDAO();
-		
+		/*
+		dao.isCPFValido(usuario.getNumeroDoCPF());
 		System.out.println("Criando DAO de cadastro");
 		dao.cadastrarNovoUsuario(usuario);
 		System.out.println("Usuario Cadastrado");
-		/*
 		System.out.println("Criando DAO de edicao");
 		dao.editarCadastroDoUsuario(usuarioTesteEditado);
 		System.out.println("Edicao Concluida");
@@ -31,8 +36,12 @@ public class UsuarioDAOTeste {
 		dao.alterarSenhaDoUsuario("11111a", usuarioTesteEditado);
 		System.out.println("Alteracao Concluida");
 		
-		 */
 		dao.validarLogin("RenatASoares@email.com", "jk5689");
+		 */
+		
+
+	        System.out.println("CPF " + cpfValido + " é válido? " + dao.isCPFValido(cpfValido));
+	        System.out.println("CPF " + cpfInvalido + " é válido? " + dao.isCPFValido(cpfInvalido));
 	}
 
 }
